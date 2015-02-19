1fe77a3f6d6f82311ca84431d354c0b95f7d1675
Fredrik Kindstrom <fredrik.kindstrom@gmail.com>
2015-02-19 13:06:12 +0100

    added first JFrame

--- src/iMatApp_G8/mainWindow.java
+++ src/iMatApp_G8/mainWindow.java
@@ -6,6 +6,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JToolBar;
+import javax.swing.JLabel;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import java.awt.Component;
+import java.awt.ComponentOrientation;
+import javax.swing.SwingConstants;
+import net.miginfocom.swing.MigLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.JMenu;
 
 public class mainWindow extends JFrame {
 
@@ -31,12 +42,34 @@ public class mainWindow extends JFrame {
 	 * Create the frame.
 	 */
 	public mainWindow() {
+		setMaximumSize(new Dimension(1000, 800));
+		setMinimumSize(new Dimension(600, 400));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		menuBar.setAlignmentY(Component.CENTER_ALIGNMENT);
+		menuBar.setAlignmentX(Component.LEFT_ALIGNMENT);
+		setJMenuBar(menuBar);
+		
+		JMenu mnArkiv = new JMenu("Arkiv");
+		menuBar.add(mnArkiv);
+		
+		JMenu mnRedigera = new JMenu("Redigera");
+		menuBar.add(mnRedigera);
+		
+		JMenu mnAffr = new JMenu("Affär");
+		menuBar.add(mnAffr);
+		
+		JMenu mnFnster = new JMenu("Fönster");
+		menuBar.add(mnFnster);
+		
+		JMenu mnHjlp = new JMenu("Hjälp");
+		menuBar.add(mnHjlp);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 	}
-
 }
